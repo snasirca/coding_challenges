757d1e48b01b97df40acf5ba19b3b3e654ca0193
Shahriyar Nasir <contact@snasir.ca>
2022-01-07 18:41:36 -0500

    Refactor while loop into each block

--- lib/coding_challenges/maximum_difference_between_increasing_elements.rb
+++ lib/coding_challenges/maximum_difference_between_increasing_elements.rb
@@ -3,10 +3,7 @@ module CodingChallenges
     def maximum_difference(nums)
       min_seen = nums[0]
       max_diff = -1
-      index = 0
-      while index < nums.length - 1
-        next_num = nums[index + 1]
-        index += 1
+      nums[1..-1].each do |next_num|
         next if next_num == min_seen
         max_diff = [max_diff, next_num - min_seen].max
         min_seen = [min_seen, next_num].min
