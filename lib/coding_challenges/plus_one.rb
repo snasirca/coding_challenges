19351b8e98d62d70538b67960513c03779680aea
Shahriyar Nasir <contact@snasir.ca>
2022-01-09 16:09:20 -0500

    Eliminate unnecessary reversal of input array

--- lib/coding_challenges/plus_one.rb
+++ lib/coding_challenges/plus_one.rb
@@ -2,8 +2,9 @@ module CodingChallenges
   class PlusOne
     def plus_one(nums)
       interpreted_integer = 0
-      nums.reverse!.each_with_index do |num, index|
-        interpreted_integer += num * 10**index
+      max_magnitude = nums.length - 1
+      nums.each_with_index do |num, index|
+        interpreted_integer += num * 10**(max_magnitude - index)
       end
       plus_one_integer = interpreted_integer + 1
       plus_one_integer.to_s.each_char.map(&:to_i)
