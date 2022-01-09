1a05de6f7adc3ef62d4c5bbeb8b7805bc47ede0b
Shahriyar Nasir <contact@snasir.ca>
2022-01-09 16:46:45 -0500

    Solve "Plus One" more efficiently

--- lib/coding_challenges/plus_one.rb
+++ lib/coding_challenges/plus_one.rb
@@ -1,13 +1,45 @@
 module CodingChallenges
   class PlusOne
-    def plus_one(nums)
+    def plus_one_1(nums)
       interpreted_integer = 0
       max_magnitude = nums.length - 1
       nums.each_with_index do |num, index|
-        interpreted_integer += num * 10**(max_magnitude - index)
+        interpreted_integer += num * 10 ** (max_magnitude - index)
       end
       plus_one_integer = interpreted_integer + 1
       plus_one_integer.to_s.each_char.map(&:to_i)
+    end
+
+    def plus_one_2(nums)
+      if nums.last == 9
+        interpreted_integer = 0
+        max_magnitude = nums.length - 1
+        nums.each_with_index do |num, index|
+          interpreted_integer += num * 10 ** (max_magnitude - index)
+        end
+        plus_one_integer = interpreted_integer + 1
+        plus_one_integer.to_s.each_char.map(&:to_i)
+      else
+        nums[nums.length - 1] += 1
+        nums
+      end
+    end
+
+    def plus_one(nums)
+      num_digits = nums.length
+      last_num_index = num_digits - 1
+
+      (0..last_num_index).each do |index|
+        backward_index = last_num_index - index
+        if nums[backward_index] == 9
+          nums[backward_index] = 0
+        else
+          nums[backward_index] += 1
+          return nums
+        end
+      end
+
+      [1] + nums
     end
   end
 end
