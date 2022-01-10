0657c8aa92083a652835a1372fc8b2b8ff221d3d
Shahriyar Nasir <contact@snasir.ca>
2022-01-10 14:03:54 -0500

    Solve "Valid Sudoku"

---
+++ lib/coding_challenges/valid_sudoku.rb
@@ -0,0 +1,44 @@
+module CodingChallenges
+  class ValidSudoku
+    # This is a brute force solution. Optimal solution should use a hashtable.
+    def is_valid_sudoku(board)
+      9.times do |row_index|
+        9.times do |column_index|
+          curr_num = board[row_index][column_index]
+          if curr_num != "."
+
+            # scan current row
+            9.times do |column_scan_index|
+              if board[row_index][column_scan_index] == curr_num
+                return false if column_scan_index != column_index
+              end
+            end
+
+            # scan current column
+            9.times do |row_scan_index|
+              if board[row_scan_index][column_index] == curr_num
+                return false if row_scan_index != row_index
+              end
+            end
+
+            # scan current 3x3 sub-box
+            subbox_row_index = row_index - (row_index % 3)
+            3.times do
+              subbox_column_index = column_index - (column_index % 3)
+              3.times do
+                if board[subbox_row_index][subbox_column_index] == curr_num &&
+                  (subbox_row_index != row_index && subbox_column_index != column_index)
+                  return false
+                end
+                subbox_column_index += 1
+              end
+              subbox_row_index += 1
+            end
+
+          end
+        end
+      end
+      true
+    end
+  end
+end
---
+++ spec/valid_sudoku_spec.rb
@@ -0,0 +1,43 @@
+require "coding_challenges/valid_sudoku"
+
+RSpec.describe CodingChallenges::ValidSudoku do
+  subject { CodingChallenges::ValidSudoku.new }
+
+  it "1" do
+    board =
+      [
+        %w[5 3 . . 7 . . . .],
+        %w[6 . . 1 9 5 . . .],
+        %w[. 9 8 . . . . 6 .],
+        %w[8 . . . 6 . . . 3],
+        %w[4 . . 8 . 3 . . 1],
+        %w[7 . . . 2 . . . 6],
+        %w[. 6 . . . . 2 8 .],
+        %w[. . . 4 1 9 . . 5],
+        %w[. . . . 8 . . 7 9]
+      ]
+
+    result = subject.is_valid_sudoku(board)
+
+    expect(result).to eq(true)
+  end
+
+  it "2" do
+    board =
+      [
+        %w[8 3 . . 7 . . . .],
+        %w[6 . . 1 9 5 . . .],
+        %w[. 9 8 . . . . 6 .],
+        %w[8 . . . 6 . . . 3],
+        %w[4 . . 8 . 3 . . 1],
+        %w[7 . . . 2 . . . 6],
+        %w[. 6 . . . . 2 8 .],
+        %w[. . . 4 1 9 . . 5],
+        %w[. . . . 8 . . 7 9]
+      ]
+
+    result = subject.is_valid_sudoku(board)
+
+    expect(result).to eq(false)
+  end
+end
