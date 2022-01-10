20bae6efd4a3a9dbf9e8ca3a3e47499d1b59e270
Shahriyar Nasir <contact@snasir.ca>
2022-01-09 22:36:08 -0500

    Solve "Best Time to Buy and Sell Stock II"

---
+++ spec/best_time_to_buy_and_sell_stock_ii_spec.rb
@@ -0,0 +1,61 @@
+require "coding_challenges/best_time_to_buy_and_sell_stock_ii"
+
+RSpec.describe CodingChallenges::BestTimeToBuyAndSellStockII do
+  subject { CodingChallenges::BestTimeToBuyAndSellStockII.new }
+
+  it "1" do
+    nums = [7, 1, 5, 3, 6, 4]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(7)
+  end
+
+  it "2" do
+    nums = [1, 2, 3, 4, 5]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(4)
+  end
+
+  it "3" do
+    nums = [7, 6, 4, 3, 1]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(0)
+  end
+
+  it "4" do
+    nums = [3, 6, 4]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(3)
+  end
+
+  it "5" do
+    nums = [6, 4]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(0)
+  end
+
+  it "6" do
+    nums = [4]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(0)
+  end
+
+  it "7" do
+    nums = [1, 2, 3, 4]
+
+    result = subject.max_profit(nums)
+
+    expect(result).to eq(3)
+  end
+end
