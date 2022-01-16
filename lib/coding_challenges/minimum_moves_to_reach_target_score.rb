4994a6c4bee244108bb0d3c0e9a67c087ef350b0
Shahriyar Nasir <contact@snasir.ca>
2022-01-15 22:46:40 -0500

    Solve "Minimum Moves to Reach Target Score"

---
+++ lib/coding_challenges/minimum_moves_to_reach_target_score.rb
@@ -0,0 +1,30 @@
+module CodingChallenges
+  class MinimumMovesToReachTargetScore
+    def min_moves(target, max_doubles)
+      result = target
+      moves = 0
+      doublings = 0
+
+      while result != 1
+
+        if doublings < max_doubles
+          if result % 2 == 0
+            doublings += 1
+            result /= 2
+            moves += 1
+            next
+          end
+        else
+          moves += result - 1
+          result = 1
+          next
+        end
+
+        result -= 1
+        moves += 1
+      end
+
+      moves
+    end
+  end
+end
---
+++ spec/minimum_moves_to_reach_target_score_spec.rb
@@ -0,0 +1,68 @@
+require "coding_challenges/minimum_moves_to_reach_target_score"
+
+RSpec.describe CodingChallenges::MinimumMovesToReachTargetScore do
+  subject { CodingChallenges::MinimumMovesToReachTargetScore.new }
+
+  it "1" do
+    target = 5
+    max_doubles = 0
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(4)
+  end
+
+  it "2" do
+    target = 19
+    max_doubles = 2
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(7)
+  end
+
+  it "3" do
+    target = 10
+    max_doubles = 4
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(4)
+  end
+
+  it "4" do
+    target = 1
+    max_doubles = 999
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(0)
+  end
+
+  it "5" do
+    target = 2
+    max_doubles = 999
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(1)
+  end
+
+  it "5" do
+    target = 3
+    max_doubles = 999
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(2)
+  end
+
+  it "6" do
+    target = 656101987
+    max_doubles = 1
+
+    result = subject.min_moves(target, max_doubles)
+
+    expect(result).to eq(328050994)
+  end
+end
