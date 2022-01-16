cb0d99b6b9d151bb1544d0764d7cbb5ff7418703
Shahriyar Nasir <contact@snasir.ca>
2022-01-15 22:22:45 -0500

    Solve "Divide a String Into Groups of Size k"

---
+++ lib/coding_challenges/divide_a_string_into_groups_of_size_k.rb
@@ -0,0 +1,24 @@
+module CodingChallenges
+  class DivideAStringIntoGroupsOfSizeK
+    def divide_string(s, k, fill)
+      l = s.length
+      indices = (l / k)
+        .times
+        .to_a
+        .map { |x| x * k }
+
+      result = []
+      indices.each do |index|
+        result << s[index,k]
+      end
+      if l % k != 0
+        if indices.empty?
+          result << s + fill * (k - (l % k))
+        else
+          result << s[(indices[-1] + k)..-1] + fill * (k - (l % k))
+        end
+      end
+      result
+    end
+  end
+end
---
+++ spec/divide_a_string_into_groups_of_size_k_spec.rb
@@ -0,0 +1,65 @@
+require "coding_challenges/divide_a_string_into_groups_of_size_k"
+
+RSpec.describe CodingChallenges::DivideAStringIntoGroupsOfSizeK do
+  subject { CodingChallenges::DivideAStringIntoGroupsOfSizeK.new }
+
+  it "1" do
+    s = "abcdefghi"
+    k = 3
+    fill = "x"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["abc", "def", "ghi"])
+  end
+
+  it "2" do
+    s = "abcdefghij"
+    k = 3
+    fill = "x"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["abc", "def", "ghi", "jxx"])
+  end
+
+  it "3" do
+    s = "ab"
+    k = 1
+    fill = "x"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["a", "b"])
+  end
+
+  it "4" do
+    s = "a"
+    k = 1
+    fill = "x"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["a"])
+  end
+
+  it "5" do
+    s = "a"
+    k = 3
+    fill = "x"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["axx"])
+  end
+
+  it "6" do
+    s = "ctoyjrwtngqwt"
+    k = 8
+    fill = "n"
+
+    result = subject.divide_string(s, k, fill)
+
+    expect(result).to eq(["ctoyjrwt", "ngqwtnnn"])
+  end
+end
