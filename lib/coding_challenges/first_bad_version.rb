6943f6db48d3da30f9e81c2d984ef537670ee51e
Shahriyar Nasir <contact@snasir.ca>
2022-01-16 21:14:18 -0500

    Improve "First Bad Version"

--- lib/coding_challenges/first_bad_version.rb
+++ lib/coding_challenges/first_bad_version.rb
@@ -11,19 +11,17 @@ module CodingChallenges
       left = 1
       right = n
 
-      return left if is_bad_version(left)
-
-      while left != right && left + 1 != right
-        version_to_check = (left + right) / 2
+      while left < right
+        version_to_check = left + ((right - left) / 2)
 
         if is_bad_version(version_to_check)
           right = version_to_check
         else
-          left = version_to_check
+          left = version_to_check + 1
         end
       end
 
-      right
+      left
     end
   end
 end
