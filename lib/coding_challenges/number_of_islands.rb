0d25d65d4739c01c381e333a04f2463caf0c59ae
Shahriyar Nasir <contact@snasir.ca>
2022-01-27 16:07:57 -0500

    Improve "Number of Islands"

--- lib/coding_challenges/number_of_islands.rb
+++ lib/coding_challenges/number_of_islands.rb
@@ -1,14 +1,17 @@
 module CodingChallenges
   class NumberOfIslands
+    Coordinate = Struct.new(:row, :col)
+
     def num_islands(grid)
       visited = Set.new
       result = 0
 
       grid.each_with_index do |row, row_index|
         row.each_with_index do |cell, col_index|
-          if !visited.include?([row_index, col_index]) && cell == "1"
+          at = Coordinate.new(row_index, col_index)
+          if not_visited(visited, at) && cell == "1"
             result += 1
-            dfs(visited, grid, [row_index, col_index])
+            dfs(visited, grid, at)
           end
         end
       end
@@ -19,8 +22,8 @@ module CodingChallenges
     def dfs(visited, grid, at)
       visited << at
       neighbours_coords(grid, at)
-        .select { |neighbour_coord| grid[neighbour_coord[0]][neighbour_coord[1]] == "1" }
-        .select { |neighbour_coord| !visited.include?(neighbour_coord) }
+        .select { |neighbour_coord| cell_in(grid, neighbour_coord) == "1" }
+        .select { |neighbour_coord| not_visited(visited, neighbour_coord) }
         .each { |island_part_coord| dfs(visited, grid, island_part_coord) }
     end
 
@@ -30,11 +33,19 @@ module CodingChallenges
       col = at[1]
       max_col = grid[0].length - 1
       result = []
-      result << [row, col + 1] if (row).between?(0, max_row) && (col + 1).between?(0, max_col)
-      result << [row + 1, col] if (row + 1).between?(0, max_row) && (col).between?(0, max_col)
-      result << [row, col - 1] if (row).between?(0, max_row) && (col - 1).between?(0, max_col)
-      result << [row - 1, col] if (row - 1).between?(0, max_row) && (col).between?(0, max_col)
+      result << Coordinate.new(row, col + 1) if (row).between?(0, max_row) && (col + 1).between?(0, max_col)
+      result << Coordinate.new(row + 1, col) if (row + 1).between?(0, max_row) && (col).between?(0, max_col)
+      result << Coordinate.new(row, col - 1) if (row).between?(0, max_row) && (col - 1).between?(0, max_col)
+      result << Coordinate.new(row - 1, col) if (row - 1).between?(0, max_row) && (col).between?(0, max_col)
       result
+    end
+
+    def cell_in(grid, coordinate)
+      grid[coordinate.row][coordinate.col]
+    end
+
+    def not_visited(visited, coordinate)
+      !visited.include?(coordinate)
     end
   end
 end
